56a8f487f03841b948d614b7d6d2454af231075b
Kate Basova <120415812+kkwwaa@users.noreply.github.com>
2025-05-16 02:02:00 +0500

    Delete monitor.sh

--- monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Server Health Check - $(date)"
-echo "Disk space:"
-df -h /
-echo "Memory Usage:"
-free -h
-echo "Nginx Status:"
-sudo systemctl status nginx --no-pager | grep Active
-
