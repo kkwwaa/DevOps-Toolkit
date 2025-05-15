c9f3d44142ec1ec74baaaab0dfad694d04a035c2
Kate Basova <120415812+kkwwaa@users.noreply.github.com>
2025-05-16 02:01:41 +0500

    Delete backup.sh

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TIMESTAMP=$(date +%F_%H-%M-%S)
-BACKUP_DIR=~/backups
-SOURCE_DIR=/var/www/html
-BACKUP_NAME=web_backup_$TIMESTAMP.tar.gz
-tar -czf $BACKUP_DIR/$BACKUP_NAME $SOURCE_DIR
-echo "Backup created: $BACKUP_NAME"
